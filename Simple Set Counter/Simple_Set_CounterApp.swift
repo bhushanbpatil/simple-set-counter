b4b08d56ca194897d44ab2f882892ada7b3a19d1
bhution <bhushanbpatil8@gmail.com>
2026-07-05 02:53:49 -0500

    Initial Commit

---
+++ Simple Set Counter/Simple_Set_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Set_CounterApp.swift
+//  Simple Set Counter
+//
+//  Created by Bhushan Patil on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Set_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
